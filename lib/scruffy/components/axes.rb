44569f55f4a598569925c31a5b6eeeb6a09fbd86
Will Bryant <will.bryant@gmail.com>
2009-12-21 16:53:17 +1300

    implemented a rudimentary axes component

---
+++ lib/scruffy/components/axes.rb
@@ -0,0 +1,18 @@
+module Scruffy
+  module Components
+    class Axes < Base
+      include Scruffy::Helpers::Marker
+      
+      def draw(svg, bounds, options={})
+        stroke_width = options[:stroke_width]
+        
+        y = (options[:max_value] * bounds[:height])/(options[:max_value] - options[:min_value])
+        svg.line(:x1 => 0, :y1 => y, :x2 => bounds[:width], :y2 => y, :style => "stroke: #{options[:theme].marker.to_s}; stroke-width: #{stroke_width};")
+        
+        x = -0.5
+        svg.line(:x1 => x, :y1 => 0, :x2 => x, :y2 => bounds[:height], :style => "stroke: #{options[:theme].marker.to_s}; stroke-width: #{stroke_width};")
+      end
+    end
+  end
+end
+
